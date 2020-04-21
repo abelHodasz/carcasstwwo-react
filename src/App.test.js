9850d3f0af1c13e37193cc43c41f6eef97bd85b3
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-04-21 13:41:01 +0200

    Setup tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("App Testing", () => {
+    test("Renders Hello World!", () => {
+        const wrapper = Enzyme.shallow(<App />);
+        expect(wrapper.find("p").text()).toContain("Hello world");
+    });
 });
