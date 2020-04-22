d5efc804fae99d6c53a317b800c27ccb0342669d
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-04-22 11:37:01 +0200

    Added signalR hub

--- src/pages/Lobby/Lobby.js
+++ src/pages/Lobby/Lobby.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "../../axios/axios";
 import { useCookies } from "react-cookie";
+import { HubConnectionBuilder } from "@aspnet/signalr";
 
 export default function Lobby(props) {
     const [joined, setJoined] = useState(false);
@@ -9,9 +10,14 @@ export default function Lobby(props) {
     const [isHost, setIsHost] = useState(false);
     const { code } = useParams();
     const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+    const [hubConnection, setHubConnection] = useState(null);
 
     useEffect(() => {
         if (cookies.user && cookies.user.isHost) setIsHost(true);
+        const hubConn = new HubConnectionBuilder()
+            .withUrl("https://localhost:5000/lobby")
+            .build();
+        setHubConnection(hubConn);
     }, []);
 
     useEffect(() => {
@@ -19,6 +25,14 @@ export default function Lobby(props) {
             setJoined(true);
         }
     }, [cookies]);
+
+    useEffect(() => {
+        if (hubConnection != null)
+            hubConnection
+                .start()
+                .then(() => console.log("Conntection started!"))
+                .catch((e) => console.log(e));
+    }, [hubConnection]);
 
     if (error)
         return (
