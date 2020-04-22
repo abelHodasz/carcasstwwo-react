f86727c0917b5f83a888bb35a336b1d01ae84376
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-04-22 11:12:26 +0200

    user cookie

--- src/pages/Lobby/Lobby.js
+++ src/pages/Lobby/Lobby.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "../../axios/axios";
+import { useCookies } from "react-cookie";
 
 export default function Lobby(props) {
     const [joined, setJoined] = useState(false);
     const [error, setError] = useState("");
+    const [isHost, setIsHost] = useState(false);
     const { code } = useParams();
+    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+        if (cookies.user && cookies.user.isHost) setIsHost(true);
+    }, []);
+
+    useEffect(() => {
+        if (cookies.user) {
+            setJoined(true);
+        }
+    }, [cookies]);
 
     if (error)
         return (
@@ -15,5 +27,5 @@ export default function Lobby(props) {
             </div>
         );
     else if (joined) return <div className="lobby"></div>;
-    else return <div className="lobby"></div>;
+    else return <div className="lobby">Waiting for server...</div>;
 }
