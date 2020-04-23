9cc1f1d2a2aa184044ccaacf785a4b7468503e99
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-04-23 11:17:53 +0200

    Connect to hub

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button } from "@material-ui/core";
+import { HubConnectionBuilder } from "@aspnet/signalr";
 
 export default function Home(props) {
     const [name, setName] = useState("");
+    const [hubConnection, setHubConnection] = useState(null);
+
+    useEffect(() => {
+        const hubConn = new HubConnectionBuilder()
+            .withUrl("http://localhost:5000/lobby")
+            .build();
+        console.log(hubConn);
+        setHubConnection(hubConn);
+    }, []);
+
+    useEffect(() => {
+        if (hubConnection != null)
+            hubConnection
+                .start()
+                .then(() => console.log("Conntection started!"))
+                .catch((e) => console.log(e));
+    }, [hubConnection]);
 
     const onCreateLobby = (event) => {
         if (name.length > 0) {
-            props.history.push("/lobby/asd123");
+            hubConnection
+                .invoke("CreateGroup")
+                .then((groupName) => {
+                    props.history.push(`/lobby/${groupName}`);
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
         }
     };
 
