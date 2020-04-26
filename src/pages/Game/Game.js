6ff502ccbca886476675a6a69d770946059a972a
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-04-26 20:22:59 +0200

    Add Test Tile

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import tile1 from "../../images/1_4.png";
 
 import ThreeService from "../../services/ThreeService";
+import Tile from "../../services/Tile";
 
 export default function Game(props) {
     const [mount, setMount] = useState(null);
@@ -8,6 +10,9 @@ export default function Game(props) {
     useEffect(() => {
         if (mount != null) {
             const three = new ThreeService(mount);
+            const tile = new Tile(tile1);
+            three.scene.add(tile.mesh);
+            tile.y = 1;
             three.init();
             three.animate();
         }
