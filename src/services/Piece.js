af612d41d60c720f98748352fadd78d5c28e423c
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-04-27 15:09:39 +0200

    Center piece, model property

--- src/services/Piece.js
+++ src/services/Piece.js
@@ -1,26 +1,34 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import piece from "../models/Meeple.gltf";
-import { Mesh, MeshLambertMaterial } from "three";
+import { Mesh, MeshLambertMaterial, Box3, Vector3 } from "three";
 
 export default class Piece {
     constructor(scene) {
         var loader = new GLTFLoader();
-        var material = new MeshLambertMaterial({ color: 0xff0000 });
-        loader.load(piece, (result) => {
+        this.material = new MeshLambertMaterial({ color: 0xff0000 });
+        this.model = null;
+        const loadMethod = (result) => {
             var model = result.scene.children[2];
-            model.position.set(0.35, 0.02, -0.55);
+            this.model = model;
             model.rotateX(0.5 * Math.PI);
             model.castShadow = true;
             model.receiveShadow = true;
-            model.traverse(function (node) {
+            var box = new Box3().setFromObject(model);
+            var center = new Vector3();
+            box.getCenter(center);
+            center.y = 0.02;
+            model.position.sub(center);
+            model.traverse((node) => {
                 if (node.isMesh || node instanceof Mesh) {
-                    node.material = material;
+                    node.material = this.material;
                     node.receiveShadow = true;
                     node.castShadow = true;
                 }
             });
 
             scene.add(model);
-        });
+        };
+
+        loader.load(piece, loadMethod);
     }
 }
