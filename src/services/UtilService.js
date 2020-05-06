61ef1c7bc08b1807f2c9899524ad88ba416570ee
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-05-06 13:41:27 +0200

    Create UtilService.js

---
+++ src/services/UtilService.js
@@ -0,0 +1,14 @@
+import { Vector3 } from "three";
+
+export function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+export function getMousePosition(camera, mouse) {
+    const mouseVector = new Vector3(mouse.x, mouse.y, 1);
+    mouseVector.unproject(camera);
+    var dir = mouseVector.sub(camera.position).normalize();
+    var distance = -camera.position.y / dir.y;
+    var pos = camera.position.clone().add(dir.multiplyScalar(distance));
+    return pos;
+}
