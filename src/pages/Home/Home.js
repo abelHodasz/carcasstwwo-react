540570be80e1906fd2206808c956b8ab5d09ae8e
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-06-03 13:48:00 +0200

    Redesigned home screen

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useContext } from "react";
-import { TextField, Button, Box } from "@material-ui/core";
+import React, { useState, useEffect, useContext, Fragment } from "react";
+import { Grid, Typography } from "@material-ui/core";
 import { HubConnectionContext } from "../../context/HubConnectionContext";
+import { TextField, Button, Box, Container } from "../../themes/components";
 import { HubConnectionBuilder } from "@aspnet/signalr";
 import Loading from "../../components/Loading/Loading";
-
+import CarcassTwoLogo from "../../svg/carcassTwoLogo.svg";
+import CarcassonneIllustration from "../../images/site/carcassonne-illustration.png";
 import "./Home.css";
 
 export default function Home(props) {
@@ -58,49 +60,95 @@ export default function Home(props) {
     };
 
     return (
-        <div className="home">
-            {loading ? (
-                <div className=".loading">
-                    <Loading />
-                </div>
-            ) : (
-                <>
-                    <Box className="row">
-                        <TextField
-                            required
-                            className="name-input"
-                            id="standard-basic"
-                            label="Your Name"
-                            onChange={(e) => {
-                                setName(e.target.value);
-                            }}
-                        ></TextField>
-                        <Button
-                            className="create-lobby-btn"
-                            onClick={onCreateLobby}
+        <Container>
+            <Box textAlign="center">
+                <Typography variant="h1">CarcassTwwo</Typography>
+            </Box>
+            <Grid container justify="center">
+                <Grid item xs={10} sm={6}>
+                    <img
+                        className="home-img"
+                        src={CarcassonneIllustration}
+                        alt="carcassonne-illustration"
+                    />
+                </Grid>
+                <Grid item xs={10} md={6}>
+                    <Box display="flex" flexDirection="column" height="100%">
+                        <Box component="section" centertext>
+                            <Typography variant="h3">
+                                Change your name
+                            </Typography>
+                            <TextField
+                                variant="outlined"
+                                className="name-input"
+                                label="Name"
+                                size="small"
+                                onChange={(e) => {
+                                    setName(e.target.value);
+                                }}
+                            ></TextField>
+                        </Box>
+                        <Box
+                            centertext
+                            component="section"
+                            flexGrow="1"
+                            display="flex"
+                            flexDirection="column"
+                            style={{ height: "100%" }}
                         >
-                            Create Lobby
-                        </Button>
+                            <Typography variant="h3">
+                                Create or Join a Lobby
+                            </Typography>
+                            <Grid
+                                container
+                                justify="center"
+                                alignItems="center"
+                            >
+                                <Grid item xs={12} sm={5}>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        className="create-lobby-btn"
+                                        onClick={onCreateLobby}
+                                    >
+                                        Create Lobby
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={12} sm={1}>
+                                    or
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <Box
+                                        display="flex"
+                                        flexDirection="column"
+                                        justifyContent="space-around"
+                                        alignItems="center"
+                                    >
+                                        <TextField
+                                            variant="outlined"
+                                            className="room-input"
+                                            id="standard-basic"
+                                            label="Room Code"
+                                            size="small"
+                                            onChange={(e) => {
+                                                setRoom(e.target.value);
+                                            }}
+                                        ></TextField>
+                                        <Button
+                                            variant="contained"
+                                            color="secondary"
+                                            className="join-lobby-btn"
+                                            onClick={onJoinLobby}
+                                        >
+                                            Join Lobby
+                                        </Button>
+                                    </Box>
+                                </Grid>
+                            </Grid>
+                        </Box>
                     </Box>
-                    <Box className="row">
-                        <TextField
-                            required
-                            className="room-input"
-                            id="standard-basic"
-                            label="Room Code"
-                            onChange={(e) => {
-                                setRoom(e.target.value);
-                            }}
-                        ></TextField>
-                        <Button
-                            className="join-lobby-btn"
-                            onClick={onJoinLobby}
-                        >
-                            Join Lobby
-                        </Button>
-                    </Box>
-                </>
-            )}
-        </div>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
