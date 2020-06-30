633fc40cafd770eb477da6142e759eb1b7736326
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-06-30 11:58:02 +0200

    Develop (#3)
    * Update axios url
    
    * Add password protection
    
    * url based on environment

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API_URL = "https://localhost:5001/api/v1";
+const API_URL =
+    process.env.NODE_ENV === "production"
+        ? "http://carcasstwwo.herokuapp.com"
+        : "http://localhost:5000";
+
 const TIMEOUT = 5000;
 
 const instance = axios.create({
